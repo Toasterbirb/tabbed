95eeb425e052e296cc0d4e8378558a2c63a496c5
Toasterbirb <git.53jvg@aleeas.com>
2021-02-07 20:32:03 +0200

    Changed font to mononoki and tweaked the colors a bit

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=12";
+static const char font[]        = "mononoki:size=15";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
-static const char* selfgcolor   = "#ffffff";
+static const char* selbgcolor   = "#ffffff";
+static const char* selfgcolor   = "#000000";
 static const char* urgbgcolor   = "#111111";
 static const char* urgfgcolor   = "#cc0000";
 static const char before[]      = "<";
